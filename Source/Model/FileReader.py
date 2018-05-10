fe7635b80d48970b65c58da08639b18970a0d485
BassamMattar <37397559+BassamMattar@users.noreply.github.com>
2018-05-10 21:13:22 +0200

    reading file

---
+++ Source/Model/FileReader.py
@@ -0,0 +1,46 @@
+class MyClass(object):
+    file = 0
+    method = 0
+    nPoints = 0
+    x = []
+    y = []
+    xQueries = []
+    def __init__(self, path = ""):
+        self.file = open(path, "r")
+        
+    def getResult(self):
+        self.x = []
+        self.y = []
+        self.xQueries = []
+
+        lines = self.file.readlines()
+
+        self.method = int(lines[0])
+        self.nPoints = int(lines[1])
+        
+        Points = lines[2]
+        
+        Points = Points.replace("[","")
+        Points = Points.replace("]","")
+        
+        array = Points.split(",")
+        
+        for i in array:
+            self.x.append(float(i))
+            
+        Points = lines[3]
+        Points = Points.replace("[","")
+        Points = Points.replace("]","")
+        array = Points.split(",")
+        
+        for i in array:
+            self.y.append(float(i))    
+               
+        Points = lines[4]
+        Points = Points.replace("[","")
+        Points = Points.replace("]","")
+        array = Points.split(",")
+        
+        for i in array:
+            self.xQueries.append(float(i))
+        return self.method,self.x,self.y,self.xQueries
